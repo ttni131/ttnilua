6fae9041adb74200974bd8508b420c7e8e5b56ad
ttni131 <meteakdag5757@gmail.com>
2026-02-28 01:54:38 +0300

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,19 @@
+--[[
+    =========================================
+    PROJECT: TTNI ROBLOX HUB
+    AUTHOR: TTNI
+    STATUS: ACTIVE
+    Everything made by TTNI.
+    =========================================
+]]
+
+-- Başlangıç Bildirimi
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "TTNI Hub";
+    Text = "Script başarıyla yüklendi! Yapımcı: TTNI";
+    Duration = 5;
+})
+
+print("TTNI Hub is starting...")
+
+-- Buraya birazdan fonksiyonları ve UI'yı ekleyeceğiz.
